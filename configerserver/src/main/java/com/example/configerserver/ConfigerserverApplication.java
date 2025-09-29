a57e2a11970de585aa46da2cde29ee914a310633
vijay GM <vijaygmvijaygmvg2003@gmail.com>
2025-09-29 14:38:27 +0530

    added configure server

---
+++ configerserver/src/main/java/com/example/configerserver/ConfigerserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.configerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigerserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigerserverApplication.class, args);
+	}
+
+}
